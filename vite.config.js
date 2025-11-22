6a33da79e97f9971f6366915978c4f6bb6f00c1e
biveshka <biveshka.kk@gmail.com>
2025-11-22 23:52:59 +0500

    59

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // ИЗМЕНИТЬ НА './'
+  base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000
   }
 })
