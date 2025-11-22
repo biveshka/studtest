d67cc9f9b6fdc88f2a8522b0998fab70d2e19d32
biveshka <biveshka.kk@gmail.com>
2025-11-23 01:16:01 +0500

    81

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // или '/' для Vercel
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
-  },
-  server: {
-    host: true,
-    port: 3000
+  base: './',
+  css: {
+    postcss: './postcss.config.js'
   }
 })
