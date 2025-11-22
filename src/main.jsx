cb9a7e653402ba16fdf8619307212b42050027e5
biveshka <biveshka.kk@gmail.com>
2025-11-23 00:28:35 +0500

    66

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
-import './App.css' // ДОБАВЬТЕ ЭТУ СТРОКУ
+import './App.css' // ДОЛЖЕН БЫТЬ ПЕРВЫМ!
+import './index.css' // ДОЛЖЕН БЫТЬ ВТОРЫМ!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
